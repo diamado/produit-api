b64ffb1c30380bcabf153359cef81593f6e13e7f
Your Name <you@example.com>
2025-07-03 00:49:37 +0000

    crud

--- src/main/java/com/produit/produitservice/Projet2ProduitApplication.java
+++ src/main/java/com/produit/produitservice/Projet2ProduitApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Projet2ProduitApplication {
+public class
+Projet2ProduitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Projet2ProduitApplication.class, args);
--- src/main/java/com/produit/produitservice/service/ProduitService.java
+++ src/main/java/com/produit/produitservice/service/ProduitService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -19,6 +20,39 @@ public class ProduitService {
 
     public Produit createProduit(Produit produit) {
         return produitRepository.save(produit);
+    }
+
+    public Produit getProduitById(long id) {
+        Optional<Produit> produitOptional= produitRepository.findById(id);
+
+        if(produitOptional.isEmpty()){
+            throw  new RuntimeException("Desolé produit");
+        }
+        return produitOptional.get();
+    }
+
+    public String deleteProduitById(long idProduit) {
+        Optional<Produit> deleteOptional= produitRepository.findById(idProduit);
+
+        if(deleteOptional.isEmpty()){
+            throw  new RuntimeException("suppression impossible");
+        }
+        produitRepository.delete(deleteOptional.get());
+        return "Produit supprimer avec succés";
+    }
+
+    public Produit editProduit(long id,Produit produit) {
+       Optional<Produit> produitOptional=produitRepository.findById(id);
+
+       if(produitOptional.isEmpty()){
+           throw new RuntimeException("modification impossible");
+
+       }
+       Produit produitAModifier = produitOptional.get();
+       produitAModifier.setName(produit.getName());
+       produitAModifier.setPrice(produit.getPrice());
+
+       return produitRepository.save(produitAModifier);
     }
 }
 
